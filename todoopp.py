cad9a20efa937f48ba18eb8ec8f27ed77d806b39
Strunzyyy <tiziuggi@gmail.com>
2026-07-08 15:14:58 +0200

    Klasse und neue Attribute hinzugefügt

--- todoopp.py
+++ todoopp.py
@@ -2,4 +2,14 @@ class Todo:
     def __init__(self, text, erledigt=False):
         self.text = text
         self.erledigt = erledigt
-        
+
+    def abhaken(self):
+        self.erledigt = True
+
+
+meine_aufgabe = Todo("Dokumentation schreiben")
+print(meine_aufgabe.erledigt)
+
+meine_aufgabe.abhaken()
+
+print(meine_aufgabe.erledigt)
